48533c5b7f4d9631c1900db5d56769872b30a3b5
Nicolas Hodicq <nicolas.hodicq@zenika.com>
2020-03-10 07:40:37 +0100

    Add Github Action and Codecov

---
+++ packages/novice/basic_layout/basic_layout_solution/android/app/src/main/kotlin/com/nartwak/basic_layout_solution/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.nartwak.basic_layout_solution
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
