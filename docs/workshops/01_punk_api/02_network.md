365194308b5a387fa4c3f5f8332da087737bbdfa
Nicolas Hodicq <nicolas.hodicq@zenika.com>
2020-11-12 10:00:15 +0100

    feat: add punk api network step

---
+++ docs/workshops/01_punk_api/02_network.md
@@ -0,0 +1,20 @@
+# 02- Network
+
+## Goal
+
+The main goal of this step is to start manipulating layout widgets, refactoring code by creating a widget that can be used more than once.
+
+You are going to:
+
+- use `Container` widget, add color background, shape and decoration
+- use `Row`, `Column` and `SingleChildScrollView` widgets
+- create private function
+- manipulate Dart `List` object and [spread operator](https://dart.dev/guides/language/language-tour#spread-operator)
+
+At the end of this step, you will have built this :point_down:
+
+<figure style="text-align: center;">
+    <img src="./resources/01_layout_goal.gif" alt="01_layout_goal.gif" style="display: inline;width: 40%"/>
+</figure>
+
+## Todo
---
+++ packages/workshops/01_punk_api/lib/02_network/models/beer.dart
@@ -0,0 +1,40 @@
+import 'package:meta/meta.dart';
+
+@immutable
+class Beer {
+  final int id;
+  final String name;
+  final String tagline;
+  final String description;
+  final String imageURL;
+
+  Beer({this.id, this.name, this.tagline, this.description, this.imageURL})
+      : assert(id != null),
+        assert(name != null),
+        assert(imageURL != null);
+
+  factory Beer.fromJson(Map<String, dynamic> json) {
+    return Beer(
+      id: json['id'] as int,
+      name: json['name'] as String,
+      tagline: json['tagline'] as String,
+      description: json['description'] as String,
+      imageURL: json['image_url'] as String,
+    );
+  }
+
+  Beer copyWith(
+      {int id,
+      String name,
+      String tagline,
+      String description,
+      String imageURL}) {
+    return Beer(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      tagline: tagline ?? this.tagline,
+      description: description ?? this.description,
+      imageURL: imageURL ?? this.imageURL,
+    );
+  }
+}
