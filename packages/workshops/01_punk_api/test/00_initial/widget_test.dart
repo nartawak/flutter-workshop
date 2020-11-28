4d7a02731d5998e8e958c58c723e3c39d99b6c92
Nicolas Hodicq <nicolas.hodicq@zenika.com>
2020-11-28 20:49:42 +0100

    fix: Add badge and fix initial step test

--- packages/workshops/01_punk_api/test/00_initial/widget_test.dart
+++ packages/workshops/01_punk_api/test/00_initial/widget_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:punk_api/01_layout/app.dart';
+import 'package:punk_api/00_initial//app.dart';
 
 void main() {
   testWidgets('Master route', (WidgetTester tester) async {
