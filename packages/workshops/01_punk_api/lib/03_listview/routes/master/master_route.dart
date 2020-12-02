fba0d9534ce627330fecccfefff95939c03a0bbc
Nicolas Hodicq <nicolas.hodicq@zenika.com>
2020-12-02 18:48:08 +0100

    feat: split step network in 2 differents steps

---
+++ packages/workshops/01_punk_api/lib/03_listview/routes/master/master_route.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:punk_api/03_listview/models/beer.dart';
+import 'package:punk_api/03_listview/repositories/beer_repository.dart';
+import 'package:punk_api/03_listview/routes/master/widgets/punkapi_card.dart';
+
+class MasterRouteStateful extends StatefulWidget {
+  final BeersRepository beersRepository;
+
+  MasterRouteStateful({@required this.beersRepository})
+      : assert(beersRepository != null);
+
+  @override
+  _MasterRouteStatefulState createState() => _MasterRouteStatefulState();
+}
+
+class _MasterRouteStatefulState extends State<MasterRouteStateful> {
+  List<Beer> _beers;
+  bool _isError = false;
+  bool _isLoading = true;
+
+  Widget _displayBody() {
+    if (_isError) {
+      return Center(
+        child: Text('An error occurred'),
+      );
+    }
+
+    if (_isLoading) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    return ListView.builder(
+      itemCount: _beers.length,
+      itemBuilder: (_, index) {
+        return Container(
+          margin: EdgeInsets.only(bottom: 10),
+          child: PunkApiCard(beer: _beers[index]),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Punk API'),
+        centerTitle: true,
+      ),
+      body: _displayBody(),
+    );
+  }
+
+  void _fetchBeers() async {
+    try {
+      final beers = await widget.beersRepository.getBeers();
+      setState(() {
+        _isError = false;
+        _isLoading = false;
+        _beers = beers;
+      });
+    } catch (e) {
+      setState(() {
+        _isError = true;
+        _isLoading = false;
+        _beers = null;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchBeers();
+  }
+}
+
+class MasterRouteFutureBuilder extends StatelessWidget {
+  final BeersRepository beersRepository;
+
+  MasterRouteFutureBuilder({@required this.beersRepository})
+      : assert(beersRepository != null);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Punk API'),
+        centerTitle: true,
+      ),
+      body: FutureBuilder(
+        future: beersRepository.getBeers(),
+        builder: (_, snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text('An error occurred'),
+            );
+          }
+
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          final beers = snapshot.data;
+
+          return ListView.builder(
+            itemCount: beers.length,
+            itemBuilder: (_, index) {
+              return Container(
+                margin: EdgeInsets.only(bottom: 10),
+                child: PunkApiCard(beer: beers[index]),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ packages/workshops/01_punk_api/lib/03_main_listview.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import '03_listview/app.dart';
+
+void main() {
+  runApp(PunkApiApp());
+}
