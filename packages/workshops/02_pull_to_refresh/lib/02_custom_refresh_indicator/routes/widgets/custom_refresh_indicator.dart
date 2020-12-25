4ba05a9bbd8b2c0b623f058a9f6ef011eb272eaf
Nicolas Hodicq <nicolas.hodicq@zenika.com>
2020-12-25 22:56:44 +0100

    fix: removed unused mode

--- packages/workshops/02_pull_to_refresh/lib/02_custom_refresh_indicator/routes/widgets/custom_refresh_indicator.dart
+++ packages/workshops/02_pull_to_refresh/lib/02_custom_refresh_indicator/routes/widgets/custom_refresh_indicator.dart
@@ -2,17 +2,6 @@ import 'package:flutter/material.dart';
 
 typedef RefreshCallback = Future<void> Function();
 
-// The state machine moves through these modes only when the scrollable
-// identified by scrollableKey has been scrolled to its min or max limit.
-enum _RefreshIndicatorMode {
-  drag, // Pointer is down.
-  armed, // Dragged far enough that an up event will run the onRefresh callback.
-  snap, // Animating to the indicator's final "displacement".
-  refresh, // Running the refresh callback.
-  done, // Animating the indicator's fade-out after refreshing.
-  canceled, // Animating the indicator's fade-out after not arming.
-}
-
 const _kEffectDuration = const Duration(milliseconds: 200);
 
 class CustomRefreshIndicator extends StatefulWidget {
